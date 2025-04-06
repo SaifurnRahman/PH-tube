cd225cdc88a3c96fd4175c6fa988c6a8a3b337dd
Saifur Rahman <saifurrahman24to7@gmail.com>
2025-04-06 22:42:43 +0600

    added modal and details

--- script/index.js
+++ script/index.js
@@ -40,6 +40,31 @@ const loadCategoryVideos = (id) => {
         })
 }
 
+const loadVideoDetails = (videoId) =>{
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video))
+}
+
+const displayVideoDetails = (video) => {
+    // console.log(video);
+
+    document.getElementById('videoDetails').showModal();
+    const detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML = `
+    <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+  </div>
+</div>
+    `
+}
 
 function displayCategories(categories) {
     // get the container
@@ -97,6 +122,7 @@ const displayVideos = (videos) => {
                     <p class="text-sm text-gray-400">${video.others.views}views</p>
                 </div>
             </div>
+            <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
         </div>
     `;
 
